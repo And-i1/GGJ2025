8a19ef53eefea6dc26872e71a46f313cc162224b
Alvaro <alvaro.bermejo@king.com>
2025-01-22 19:08:20 +0100

    Disabled gitlab ci

--- things/gyro/gyro.gd
+++ things/gyro/gyro.gd
@@ -4,18 +4,19 @@ var _dev_orientation = JavaScriptBridge.create_callback(on_device_orientation)
 var _permission_cb = JavaScriptBridge.create_callback(asked_for_permission)
 
 func _ready() -> void:
+	print("on ready")
 	var window = JavaScriptBridge.get_interface("window")
 	var dev_orientation = JavaScriptBridge.get_interface("DeviceOrientationEvent")
 	if JavaScriptBridge.eval("typeof DeviceOrientationEvent.requestPermission === 'function'"):
 		# ask for permission
 		dev_orientation.requestPermission().then(_permission_cb)
 	else:
+		print("No permission needed")
 		window.ondeviceorientation = _dev_orientation
 	
 func on_device_orientation(args) -> void:
 	var rot = args[0]
-	print("{} {} {}".format([rot.alpha, rot.alpha, rot.gamma]))
-	$CenterContainer/Label.text = "{} {} {}".format([rot.alpha, rot.alpha, rot.gamma], "{}")
+	$CenterContainer/Label.text = "{} {} {}".format([rot.alpha, rot.beta, rot.gamma], "{}")
 
 func asked_for_permission(args) -> void:
 	var permission_state = args[0]
