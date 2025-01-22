22bdba08671e177fc0cfed0f1012bf5bb4c6020f
Alvaro <alvaro.bermejo@king.com>
2025-01-22 19:35:44 +0100

    shuffled stuff

--- project/things/gyro/gyro.gd
+++ project/things/gyro/gyro.gd
@@ -4,23 +4,24 @@ var _dev_orientation = JavaScriptBridge.create_callback(on_device_orientation)
 var _permission_cb = JavaScriptBridge.create_callback(asked_for_permission)
 
 func _ready() -> void:
-	print("on ready")
+	$CenterContainer/Label.text = "on ready "
 	var window = JavaScriptBridge.get_interface("window")
 	var dev_orientation = JavaScriptBridge.get_interface("DeviceOrientationEvent")
 	if JavaScriptBridge.eval("typeof DeviceOrientationEvent.requestPermission === 'function'"):
-		# ask for permission
+		$CenterContainer/Label.text += "Permission needed "
 		dev_orientation.requestPermission().then(_permission_cb)
 	else:
-		print("No permission needed")
+		$CenterContainer/Label.text += "No permission needed "
 		window.ondeviceorientation = _dev_orientation
 	
 func on_device_orientation(args) -> void:
 	var rot = args[0]
-	$CenterContainer/Label.text = "{} {} {}".format([rot.alpha, rot.beta, rot.gamma], "{}")
+	# $CenterContainer/Label.text = "{} {} {}".format([rot.alpha, rot.beta, rot.gamma], "{}")
 
 func asked_for_permission(args) -> void:
 	var permission_state = args[0]
 	if permission_state == "granted":
+		$CenterContainer/Label.text += "Permission granted for DeviceOrientation "
 		JavaScriptBridge.get_interface("window").ondeviceorientation = _dev_orientation
 	else:
-		printerr("Permission not granted for DeviceOrientation")
+		$CenterContainer/Label.text += "Permission not granted for DeviceOrientation "
