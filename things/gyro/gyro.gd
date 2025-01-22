4c7f862ec4c1767fde845147c7431d73bba3c63a
Alvaro <alvaro.bermejo@king.com>
2025-01-22 18:59:13 +0100

    gyro print

--- things/gyro/gyro.gd
+++ things/gyro/gyro.gd
@@ -1,9 +1,25 @@
 extends Node
 
+var _dev_orientation = JavaScriptBridge.create_callback(on_device_orientation)
+var _permission_cb = JavaScriptBridge.create_callback(asked_for_permission)
 
 func _ready() -> void:
 	var window = JavaScriptBridge.get_interface("window")
-	window.ondeviceorientation = print_orientation
+	var dev_orientation = JavaScriptBridge.get_interface("DeviceOrientationEvent")
+	if JavaScriptBridge.eval("typeof DeviceOrientationEvent.requestPermission === 'function'"):
+		# ask for permission
+		dev_orientation.requestPermission().then(_permission_cb)
+	else:
+		window.ondeviceorientation = _dev_orientation
 	
-func print_orientation(event) -> void:
-	$CenterContainer/Label.text = str(event)
+func on_device_orientation(args) -> void:
+	var rot = args[0]
+	print("{} {} {}".format([rot.alpha, rot.alpha, rot.gamma]))
+	$CenterContainer/Label.text = "{} {} {}".format([rot.alpha, rot.alpha, rot.gamma], "{}")
+
+func asked_for_permission(args) -> void:
+	var permission_state = args[0]
+	if permission_state == "granted":
+		JavaScriptBridge.get_interface("window").ondeviceorientation = _dev_orientation
+	else:
+		printerr("Permission not granted for DeviceOrientation")
