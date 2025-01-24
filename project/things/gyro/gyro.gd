762e9e841579bbb6a8031c7b3365299ecc9b01b6
Alvaro <alvaro.bermejo@king.com>
2025-01-24 20:48:22 +0100

    Inverting again

--- project/things/gyro/gyro.gd
+++ project/things/gyro/gyro.gd
@@ -16,7 +16,7 @@ func on_device_orientation(args) -> void:
 	# $Ball.position = Vector2(xx, yy)
 	var xx = remap(x, 0, 180, -30, 30)
 	var yy = remap(y, 0, 180, -30, 30)
-	$Plane.rotation_degrees = Vector3(0, 0, -xx)
+	$Plane.rotation_degrees = Vector3(0, 0, xx)
 	$Label.text = "x = {}\ny = {}".format([xx, yy], "{}")
 
 func _input(event):
