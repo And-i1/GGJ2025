197579d1ea78fb0b402327c7c25b856c4a4c86c4
Alvaro <alvaro.bermejo@king.com>
2025-01-24 21:02:22 +0100

    Updated decimals

--- project/things/gyro/gyro.gd
+++ project/things/gyro/gyro.gd
@@ -18,7 +18,7 @@ func on_device_orientation(args) -> void:
 	var yy = remap(y, 0, 180, -30, 30)
 	# $Plane.rotation_degrees = Vector3(0, 0, xx)
 	$Plane.rotation_degrees = Vector3(-xx, 0, yy)
-	$Label.text = "x = {}\ny = {}".format([xx, yy], "{}")
+	$Label.text = "x = {:.2f}\ny = {:.2f}".format([xx, yy], "{}")
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
