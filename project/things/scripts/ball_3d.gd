2971a455baf6e0d0f220e7b6b7147547b7ed2448
Andy <and_i_who@proton.me>
2025-01-25 10:26:45 +0100

    fix

--- project/things/scripts/ball_3d.gd
+++ project/things/scripts/ball_3d.gd
@@ -1,4 +1,4 @@
 extends RigidBody3D
 
 func _process(delta):
-	$Camera3D.translate(global_position-$Camera3D.global_position)
+	$Camera3D.translate(Vector3(global_position.x-$Camera3D.global_position.x,global_position.y-$Camera3D.global_position.y+3,global_position.z-$Camera3D.global_position.z))
