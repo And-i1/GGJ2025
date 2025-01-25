d378e6fb11a0847819160d6223d2f63c7f3e70de
Andy <and_i_who@proton.me>
2025-01-25 10:08:24 +0100

    fix camera?

--- project/things/scripts/followcamera.gd
+++ project/things/scripts/followcamera.gd
@@ -2,4 +2,5 @@ extends Camera3D
 
 func _process(delta):
 	position.x = get_parent().get_node("Ball3D").position.x
-	position.y = get_parent().get_node("Ball3D").position.y
+	position.y = get_parent().get_node("Ball3D").position.y + 3
+	position.z = get_parent().get_node("Ball3D").position.z
