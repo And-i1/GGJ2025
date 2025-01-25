4e879c7d128e23fa8bd2dd74b173072774922554
Andy <and_i_who@proton.me>
2025-01-25 11:28:04 +0100

    fix

--- project/things/scripts/ball_3d.gd
+++ project/things/scripts/ball_3d.gd
@@ -1,4 +1,4 @@
 extends RigidBody3D
 
 func _process(delta):
-	$CameraPivot.translate(global_position-$CameraPivot.global_position)
+	$CameraPivot.translate(global_position - $CameraPivot.global_position)
