d6881b679d5e8c26975ba1e199901060bed19263
Alvaro <alvaro.bermejo@king.com>
2025-01-25 10:21:13 +0100

    Testing bg

--- project/things/lofi/lofi.gd
+++ project/things/lofi/lofi.gd
@@ -1,13 +1,15 @@
 extends Node
 
 var x_basis = 180
-var y_basis = 180
+var y_basis = 320
 
-func _ready() -> void:
+func _process(_delta: float) -> void:
 	var size = get_viewport().size
-	$Debug.text = "{}x{}".format([size[0], size[1]], "{}")
 	var max_x_scaling = size[0] / x_basis
 	var max_y_scaling = size[1] / y_basis
 	var min_scale = min(max_x_scaling, max_y_scaling)
-	$SubViewportContainer.size = Vector2()
+	$CenterContainer/SubViewportContainer.size = Vector2(x_basis * min_scale, y_basis * min_scale)
+	$CenterContainer/SubViewportContainer.scale = Vector2(min_scale, min_scale)
+	$CenterContainer/SubViewportContainer/SubViewport.size = Vector2(x_basis, y_basis)
+	$Debug.text = "{}x{}\ns={}".format([size[0], size[1], min_scale], "{}")
 	
