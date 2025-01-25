e74ee36159adcd6543a2df2d44f4a601e7539435
Alvaro <alvaro.bermejo@king.com>
2025-01-25 10:45:20 +0100

    Commenting

--- project/things/lofi/lofi.gd
+++ project/things/lofi/lofi.gd
@@ -9,7 +9,7 @@ func _process(_delta: float) -> void:
 	var max_y_scaling = size[1] / y_basis
 	var min_scale = min(max_x_scaling, max_y_scaling)
 	$SubViewportContainer.size = Vector2(x_basis * min_scale, y_basis * min_scale)
-	$SubViewportContainer.position = Vector2i(x_basis, y_basis) * min_scale)
+	# $SubViewportContainer.position = Vector2i(x_basis, y_basis) * min_scale)
 	$SubViewportContainer.scale = Vector2(min_scale, min_scale)
 	$SubViewportContainer/SubViewport.size = Vector2(x_basis, y_basis)
 	$Debug.text = "{}x{}\ns={}\n{}\n{}".format([size[0], size[1], min_scale, $SubViewportContainer.position[0], $SubViewportContainer.position[1]], "{}")
