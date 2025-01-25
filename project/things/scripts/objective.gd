e3e5d96526419276db168b59c510ca63d8ccb858
Andy <and_i_who@proton.me>
2025-01-25 10:58:38 +0100

    fix objective

--- project/things/scripts/objective.gd
+++ project/things/scripts/objective.gd
@@ -2,6 +2,6 @@ extends Node3D
 
 @export var scn : PackedScene
 
-func _on_area_3d_body_entered(body):
-	if "ball" in body.name:
+func _on_area_3d_area_entered(area):
+	if "ball" in area.name:
 		get_tree().change_scene_to_packed(scn)
