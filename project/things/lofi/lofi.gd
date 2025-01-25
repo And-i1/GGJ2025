96a53ad3d80663e8f2fadbf4eec6e0efaccfb23c
Alvaro <alvaro.bermejo@king.com>
2025-01-25 10:36:59 +0100

    Debugging pixel filter

--- project/things/lofi/lofi.gd
+++ project/things/lofi/lofi.gd
@@ -9,8 +9,8 @@ func _process(_delta: float) -> void:
 	var max_y_scaling = size[1] / y_basis
 	var min_scale = min(max_x_scaling, max_y_scaling)
 	$SubViewportContainer.size = Vector2(x_basis * min_scale, y_basis * min_scale)
-	$SubViewportContainer.size = Vector2(x_basis * min_scale, y_basis * min_scale)
+	$SubViewportContainer.position = size / 2
 	$SubViewportContainer.scale = Vector2(min_scale, min_scale)
 	$SubViewportContainer/SubViewport.size = Vector2(x_basis, y_basis)
-	$Debug.text = "{}x{}\ns={}".format([size[0], size[1], min_scale], "{}")
+	$Debug.text = "{}x{}\ns={}\n{}\n{}".format([size[0], size[1], min_scale, $SubViewportContainer.position[0], $SubViewportContainer.position[1]], "{}")
 	
