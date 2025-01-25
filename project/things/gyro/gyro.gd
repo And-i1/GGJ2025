c732e44ea38a6c170125af3496b6cb4fedf8890c
Andy <and_i_who@proton.me>
2025-01-25 01:19:06 +0100

    fix debug reference

--- project/things/gyro/gyro.gd
+++ project/things/gyro/gyro.gd
@@ -23,7 +23,7 @@ func on_device_orientation(args) -> void:
 	var yy = curve.sample(y/180)
 	$Plane.rotation_degrees = Vector3(xx, 0, yy)
 	
-	$Debug.text = "x = {}\ny = {}".format(["%.2f" % xx, "%.2f" % yy], "{}")
+	#$Debug.text = "x = {}\ny = {}".format(["%.2f" % xx, "%.2f" % yy], "{}")
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
