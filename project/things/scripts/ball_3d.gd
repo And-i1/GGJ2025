71ffdc2dbc4af4151bf392b25f86cd1c3a5e8c54
Andy <and_i_who@proton.me>
2025-01-25 10:20:37 +0100

    camera?

---
+++ project/things/scripts/ball_3d.gd
@@ -0,0 +1,4 @@
+extends RigidBody3D
+
+func _process(delta):
+	$Camera3D.translate(global_position-$Camera3D.global_position)
--- project/things/scripts/followcamera.gd
+++
@@ -1,6 +0,0 @@
-extends Camera3D
-
-func _process(delta):
-	position.x = get_parent().get_node("Ball3D").position.x
-	position.y = get_parent().get_node("Ball3D").position.y + 3
-	position.z = get_parent().get_node("Ball3D").position.z
