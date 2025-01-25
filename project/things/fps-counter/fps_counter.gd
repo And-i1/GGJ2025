c56f78b9cab4dea692249448723b4ea74de3d995
Alvaro <alvaro.bermejo@king.com>
2025-01-25 16:58:57 +0100

    Loooots of 3d work

--- project/things/fps-counter/fps_counter.gd
+++ project/things/fps-counter/fps_counter.gd
@@ -1,11 +1,17 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
+var delta_array := PackedFloat32Array()
+var current_idx : int = 0
+var moving_total : float = 0
+
 func _ready() -> void:
-	pass # Replace with function body.
+	delta_array.resize(100)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	moving_total -= delta_array[current_idx]
+	moving_total += delta
+	delta_array[current_idx] = delta
+	current_idx = (current_idx + 1) % 100
+	$Debug.text = "{} fps".format(["%.2f" % (1 / (moving_total / 100))], "{}")
