019711a77aad92255e375e8da0699a9b89bdc01d
Andy <and_i_who@proton.me>
2025-01-25 13:34:31 +0100

    reactivate objective

--- project/things/scripts/objective.gd
+++ project/things/scripts/objective.gd
@@ -1,8 +1,7 @@
 extends Node3D
 
 @export var scn : PackedScene
-signal change_scene
 
 func _on_area_3d_area_entered(area):
 	if "ball" in area.name:
-		change_scene.emit()
+		get_tree().change_scene_to_packed(scn)
