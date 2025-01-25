d1a653654d1ad7687c281a50a9750523f4a27764
Alvaro <alvaro.bermejo@king.com>
2025-01-25 10:59:11 +0100

    Debugging resizes

--- project/things/lofi/lofi.gd
+++ project/things/lofi/lofi.gd
@@ -5,11 +5,11 @@ var basis := Vector2i(180, 320)
 
 func _process(_delta: float) -> void:
 	var vsize := DisplayServer.window_get_size()
-	var max_scaled := vsize / basis
-	var min_scale := min(max_scale)
-	size = Vector2(x_basis * min_scale, y_basis * min_scale)
-	position = (size - Vector2i(x_basis, y_basis) * min_scale) / 2
-	$SubViewportContainer.scale = Vector2(min_scale, min_scale)
-	$SubViewportContainer/SubViewport.size = Vector2(x_basis, y_basis)
-	$Debug.text = "{}x{}\ns={}\n{}\n{}".format([size[0], size[1], min_scale, $SubViewportContainer.position[0], $SubViewportContainer.position[1]], "{}")
-	
+	var max_factor := vsize / basis
+	var min_factor : int = min(max_factor[0], max_factor[1])
+	size = Vector2(basis * min_factor)
+	position = (Vector2(vsize) - size) / 2
+	scale = Vector2(min_factor, min_factor)
+	$SubViewport.size = basis
+	print("{}x{} - {}\n\n".format([vsize[0], vsize[1], min_factor], "{}"))
+	$Debug.text = "{}x{} - {}\n\n".format([vsize[0], vsize[1], min_factor], "{}")
