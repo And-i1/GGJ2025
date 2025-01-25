8e230e4e7e0665add23f0befcd04fdb768547fff
Alvaro <alvaro.bermejo@king.com>
2025-01-25 10:51:48 +0100

    Type hinting

--- project/things/lofi/lofi.gd
+++ project/things/lofi/lofi.gd
@@ -1,15 +1,14 @@
-extends Node
+extends SubViewportContainer
 
-var x_basis = 180
-var y_basis = 320
+
+var basis := Vector2i(180, 320)
 
 func _process(_delta: float) -> void:
-	var size = get_viewport().size
-	var max_x_scaling = size[0] / x_basis
-	var max_y_scaling = size[1] / y_basis
-	var min_scale = min(max_x_scaling, max_y_scaling)
-	$SubViewportContainer.size = Vector2(x_basis * min_scale, y_basis * min_scale)
-	# $SubViewportContainer.position = Vector2i(x_basis, y_basis) * min_scale)
+	var vsize := DisplayServer.window_get_size()
+	var max_scaled := vsize / basis
+	var min_scale := min(max_scale)
+	size = Vector2(x_basis * min_scale, y_basis * min_scale)
+	position = (size - Vector2i(x_basis, y_basis) * min_scale) / 2
 	$SubViewportContainer.scale = Vector2(min_scale, min_scale)
 	$SubViewportContainer/SubViewport.size = Vector2(x_basis, y_basis)
 	$Debug.text = "{}x{}\ns={}\n{}\n{}".format([size[0], size[1], min_scale, $SubViewportContainer.position[0], $SubViewportContainer.position[1]], "{}")
