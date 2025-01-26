e0057c8a7f863ca397b6807f3df888e46497e989
Andy <and_i_who@proton.me>
2025-01-26 01:17:59 +0100

    transition

--- project/scripts/transition.gd
+++ project/scripts/transition.gd
@@ -6,3 +6,6 @@ func _ready() -> void:
 	tw.set_trans(Tween.TRANS_SINE)
 	tw.tween_property(self, "color:a", 1, 1)
 	tw.tween_property(self, "color:a", 0, 1)
+	var timer = get_tree().create_timer(1)
+	await timer.timeout
+	get_tree().change_scene_to_packed(Global.ship)
--- project/things/scripts/objective.gd
+++ project/things/scripts/objective.gd
@@ -5,4 +5,5 @@ extends Node3D
 func _on_area_3d_area_entered(area):
 	if "ball" in area.name:
 		Global.lvl += 1
-		get_tree().change_scene_to_packed(preload("res://scenes/transition.tscn"))
+		get_parent().add_sibling(preload("res://scenes/transition.tscn").instantiate())
+		
