140d119011ebbc2093d374fed2049628fca20299
Andy <and_i_who@proton.me>
2025-01-26 12:06:50 +0100

    fix double lvl detecting

--- project/things/scripts/objective.gd
+++ project/things/scripts/objective.gd
@@ -5,3 +5,4 @@ func _on_area_3d_area_entered(area):
 	if "ball" in area.name:
 		Global.lvl += 1
 		Global.add_sibling(preload("res://scenes/transition.tscn").instantiate())
+		$Area3D.monitoring = false
