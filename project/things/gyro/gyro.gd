32e655378a42b1a9e9d29a1d7275ed3434eb6022
Andy <and_i_who@proton.me>
2025-01-26 13:11:35 +0100

    distance scaling

--- project/things/gyro/gyro.gd
+++ project/things/gyro/gyro.gd
@@ -4,6 +4,7 @@ var _dev_orientation
 var curve = load("res://things/curve/new_curve.tres")
 @export var scn : PackedScene
 @export var turnstrength : float = 1
+@export var extradistancescaling : bool = false
 @onready var ball = $Ball3D
 var rotationobjective = Vector3.ZERO
 
@@ -23,6 +24,8 @@ func on_device_orientation(args) -> void:
 	# $Ball.position = Vector2(xx, yy)
 	#var xx = remap(x, 0, 180, -30, 30)
 	#var yy = remap(y, 0, 180, -30, 30)
+	if extradistancescaling:
+		turnstrength = turnstrength*(1-($Plane.global_position.distance_to(ball.global_position)/10))
 	var xx = curve.sample(x/180)*turnstrength
 	var yy = curve.sample(y/180)*turnstrength
 	rotationobjective = Vector3(xx, 0, yy)
