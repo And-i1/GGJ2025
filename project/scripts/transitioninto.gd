4ddef313a8dfd6008e15b1780503b89ee3217b40
Andy <and_i_who@proton.me>
2025-01-26 10:25:45 +0100

    change lvl loading

--- project/scripts/transitioninto.gd
+++ project/scripts/transitioninto.gd
@@ -7,8 +7,9 @@ func _ready() -> void:
 	tw.tween_property(self, "color:a", 1, 1)
 	tw.tween_property(self, "color:a", 0, 1)
 	var timer = get_tree().create_timer(1)
-	await timer.timeout
-	get_tree().change_scene_to_packed(load(Global.lvldirectories[Global.lvl]))
+	await timer.timeo
+	var scene = load(Global.lvldirectories[Global.lvl])
+	get_tree().change_scene_to_packed(scene)
 	timer = get_tree().create_timer(1)
 	await timer.timeout
 	queue_free()
